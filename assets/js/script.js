6f543d592f56feff87be3594760537f257a22610
alisson <alissonavalos@gmail.com>
2024-03-05 08:55:40 -0300

    se agregan funciones de mouse y alert a script

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,29 @@
  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
   
+ $(function(){
+
+    //se agregan funciones mouseenter  y mouseout en quienes somos 
+    $("#quienes-somos").on("mouseenter", function(){
+        $(this).css("color","red");
+    })
+    $("#quienes-somos").on("mouseout", function(){
+        $(this).css("color","white");
+    })
+
+    //boton desaparece al hacer click en enviar
+    $(".btn").on("click", function(){
+        $(".btn").toggle("slow");
+    })
+    
+    //alert al boton enviar
+    $("#boton").one("click", function(){
+        alert("gracias pr contactarte con nosotros");
+    });
+ 
+
+
+
+      
+
+ })
